d99c4981a2ef008de8011efdcdb5027d78cedec5
artbigtema <artbigtema@ya.ru>
2014-04-09 20:08:28 +0400

    add fix turn off network

--- PlayByURL/src/com/assignment1/playbyurl/MP3playActivity.java
+++ PlayByURL/src/com/assignment1/playbyurl/MP3playActivity.java
@@ -5,9 +5,12 @@ import com.assignment1.playbyurl.R;
 import java.io.File;
 
 import android.app.Activity;
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -17,12 +20,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class MP3playActivity extends Activity implements ListenerOnComplete {
+public class MP3playActivity extends Activity implements
+		DownloadMusicfromInternet.ListenerOnCompleteDownload {
 	public static final String FILE_PATH_NAME = Environment
 			.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
 			.getPath()
 			+ "/ja_ho.mp3";
-	public static final String FILE_URL = "http://android.programmerguru.com/wp-content/uploads/2014/01/jai_ho.mp3";
+	public static final String FILE_URL = "http://cs1-51v4.vk.me/p8/4109717d62ef07.mp3";// "http://android.programmerguru.com/wp-content/uploads/2014/01/jai_ho.mp3";
 	private ToggleButton btnPlayPauseMusic;
 	private MediaPlayer musicPlayer;
 	private TextView statusOfFileTextView;
@@ -32,30 +36,35 @@ public class MP3playActivity extends Activity implements ListenerOnComplete {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.a_mp3_download_play);
+
 		btnPlayPauseMusic = (ToggleButton) findViewById(R.id.btn_play_pause_music);
 		statusOfFileTextView = (TextView) findViewById(R.id.status_of_file_textview);
 		statusOfFileTextView.setText(R.string.idle);
-		DownloadMusicfromInternet downloadMusic = (DownloadMusicfromInternet) new DownloadMusicfromInternet(
-				this).execute(FILE_URL, FILE_PATH_NAME);
-		downloadMusic.setListener(this);
-		statusOfFileTextView.setText(R.string.downloading);
+		if (!isOnline()) {
+			doErrActions("Error. Turn on wifi & Restart app, please");
+		} else {
+			DownloadMusicfromInternet downloadMusic = (DownloadMusicfromInternet) new DownloadMusicfromInternet(
+					this).execute(FILE_URL, FILE_PATH_NAME);
+			downloadMusic.setListener(this);
+			statusOfFileTextView.setText(R.string.downloading);
 
-		btnPlayPauseMusic.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				if (btnPlayPauseMusic.isChecked()) {
-					if (musicPlayer == null) {
-						playMusicFile();
+			btnPlayPauseMusic.setOnClickListener(new Button.OnClickListener() {
+				public void onClick(View v) {
+					if (btnPlayPauseMusic.isChecked()) {
+						if (musicPlayer == null) {
+							playMusicFile();
+						}
+						musicPlayer.start();
+						statusOfFileTextView.setText(R.string.playing);
+						btnPlayPauseMusic.setChecked(true);
+					} else {
+						statusOfFileTextView.setText(R.string.pausing);
+						musicPlayer.pause();
+						btnPlayPauseMusic.setChecked(false);
 					}
-					musicPlayer.start();
-					statusOfFileTextView.setText(R.string.playing);
-					btnPlayPauseMusic.setChecked(true);
-				} else {
-					statusOfFileTextView.setText(R.string.pausing);
-					musicPlayer.pause();
-					btnPlayPauseMusic.setChecked(false);
 				}
-			}
-		});
+			});
+		}
 	}
 
 	@Override
@@ -92,9 +101,24 @@ public class MP3playActivity extends Activity implements ListenerOnComplete {
 		}
 	}
 
+	public boolean isOnline() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
+
 	@Override
 	public void doFinalActions() {
 		btnPlayPauseMusic.setEnabled(true);
 		statusOfFileTextView.setText(R.string.complete);
+	}
+
+	@Override
+	public void doErrActions(String message) {
+		btnPlayPauseMusic.setEnabled(false);
+		statusOfFileTextView.setText(message);
 	}
 }
