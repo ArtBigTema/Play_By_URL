e882aca564299b0b8aaa35b8944358b89b27a51c
artbigtema <artbigtema@ya.ru>
2014-04-14 19:43:17 +0400

    Revert "add name error"
    This reverts commit 25c24b8390e862a23894128aafbde33c7e44c159.

--- PlayByURL/gen/com/assignment1/playbyurl/R.java
+++ PlayByURL/gen/com/assignment1/playbyurl/R.java
@@ -1246,7 +1246,6 @@ containing a value of this type.
         public static final int app_name=0x7f0a000f;
         public static final int complete=0x7f0a0016;
         public static final int downloading=0x7f0a0013;
-        public static final int error=0x7f0a0019;
         public static final int hello_world=0x7f0a000d;
         public static final int idle=0x7f0a0012;
         public static final int pause=0x7f0a0011;
